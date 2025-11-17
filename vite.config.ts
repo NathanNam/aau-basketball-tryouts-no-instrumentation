26fdb5bf0012380f831fa01ff59f6c8e7865f9f5
Nathan Nam <nathan.nam@observeinc.com>
2025-11-17 15:20:13 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import viteReact from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     port: 3001,
+    host: '0.0.0.0',
+    allowedHosts: ['aau-basketball-tryouts.observe-demo.com'],
   },
   plugins: [
     tsConfigPaths({
